28491649fb81747ebb26593d036dd45245a49fea
irwin <irwinlkl@gmail.com>
2022-08-23 01:42:09 +0800

    fix indentation logic

--- main.py
+++ main.py
@@ -40,8 +40,10 @@ def main():
         while True:
             
             fuc.clear_screen()
-            print(len(ai_units))
             
+            
+
+
 
 
             #setting up turn base attack player go first
@@ -73,47 +75,39 @@ def main():
                 #process atk calculation
                 player_units[player_unit_to_atk].attack(ai_units[ai_unit_to_atk])
                 fuc.show_game_board(player_units,ai_units)
-
-                #if player unit die remove unit from list 
-                
-                if fuc.check_hp_is_zero(player_units) == True:
-
-                    dead_unit = fuc.dead_unit_index(player_units)
-                    player_units.pop(dead_unit)
-                
-                #check if player win
-                if len(ai_units)<= 0:
-                    print("player WIN")
-                    break
-
-                
+            
 
             else:
                 
                 ai_units[random.randint(0,len(ai_units)-1)].attack(player_units[random.randint(0,len(ai_units)-1)])  
                 fuc.show_game_board(player_units,ai_units)  
 
-                #if ai unit die remove unit from list 
                 
-                if fuc.check_hp_is_zero(ai_units) == True:
+                
 
-                    dead_unit = fuc.dead_unit_index(ai_units)
-                    ai_units.pop(dead_unit)
-                #check if ai win
-                if len(player_units)<= 0:
-                    print("Ai WIN")
-                    break
+        #if player unit or ai die remove unit from list 
+                
+            if fuc.check_hp_is_zero(player_units) == True:
+                dead_unit = fuc.dead_unit_index(player_units)
+                player_units.pop(dead_unit)            
+                
+            
+            elif fuc.check_hp_is_zero(ai_units) == True:
+                dead_unit = fuc.dead_unit_index(ai_units)
+                ai_units.pop(dead_unit)
+                
+            #check if ai win
+            if len(player_units)<= 0:
+                print("Ai WIN")
+                break   
+            
+            #check if player win
+            if len(ai_units)<= 0:
+                print("player WIN")
+                break
+            
 
-
             
-            
-            
-            
-            
-            #print(ai_life)
-            #print(player_life)
-            #print(ahp,php)
-            #print(rounds)
             rounds += 1
             p = str(input("press enter to continue:"))
             
